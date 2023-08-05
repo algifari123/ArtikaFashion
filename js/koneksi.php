79673ddffb223f13ca7e1ed1b0de8d43160222e3
algifari123 <52127503+algifari123@users.noreply.github.com>
2023-08-05 13:47:35 +0700

    Delete js directory

--- js/koneksi.php
+++
@@ -1,8 +0,0 @@
-<?php
-$con = mysqli_connect("localhost", "root", "", "toko_online");
-
-//check connection
-if (mysqli_connect_errno()) {
-    echo "Gagal menghubungkan ke database: " . mysqli_connect_error();
-    exit();
-}
